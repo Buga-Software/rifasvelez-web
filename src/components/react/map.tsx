62447ee4e97b06c0dbf50b9f9b99789d59f866ff
Gothsec <oscarandreshernandezpineda@gmail.com>
2025-07-21 13:24:19 -0500

    Change(components): small details UI

--- src/components/react/map.tsx
+++ src/components/react/map.tsx
@@ -1,28 +1,28 @@
-import { useEffect } from 'react';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+import { useEffect } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 export default function MapComponent() {
   useEffect(() => {
-    const mapExists = document.getElementById('map')?.innerHTML !== '';
+    const mapExists = document.getElementById("map")?.innerHTML !== "";
     if (mapExists) return;
 
-    const map = L.map('map').setView([3.9073432, -76.2985562], 16);
+    const map = L.map("map").setView([3.9073432, -76.2985562], 15);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors',
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "© OpenStreetMap",
     }).addTo(map);
 
     L.marker([3.9073432, -76.2985562])
       .addTo(map)
-      .bindPopup('¡Nuestra oficina!')
+      .bindPopup("¡Nuestra oficina!")
       .openPopup();
   }, []);
 
   return (
     <div
       id="map"
-      style={{ height: '100%', width: '100%', borderRadius: '12px', zIndex: 0 }}
+      style={{ height: "100%", width: "100%", borderRadius: "12px", zIndex: 0 }}
     />
   );
 }
