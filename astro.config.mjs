e4f69b1a04c4acec5ee9e8c2b6b469b39226faad
Gothsec <oscarandreshernandezpineda@gmail.com>
2025-07-28 17:40:41 -0500

    Change: Config to Google Tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,19 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import partytown from '@astrojs/partytown'
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(),
+    partytown({
+            config: {
+              forward: ["dataLayer.push"],
+            },
+  })],
 
   server: {
     host: true,
