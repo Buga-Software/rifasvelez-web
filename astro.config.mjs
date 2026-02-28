de9a2c909287cbe02131d05202fa7e6339046af1
Gothsec <oscarandreshernandezpineda@gmail.com>
2026-02-27 22:22:04 -0500

    Add: SEO Improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,35 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
-import partytown from '@astrojs/partytown'
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://rifasvelez.com",
+
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react(),
+  integrations: [
+    react(),
     partytown({
-            config: {
-              forward: ["dataLayer.push"],
-            },
-  })],
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      i18n: {
+        defaultLocale: "es",
+        locales: {
+          es: "es-CO",
+        },
+      },
+    }),
+  ],
 
   server: {
     host: true,
